2ffce932e3ff75a8c56bde79353566150cb3193b
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-01-09 08:30:44 -0300

    feat:register initial forms

--- libs/ui/shared/src/lib/register/register.component.ts
+++ libs/ui/shared/src/lib/register/register.component.ts
@@ -1,8 +1,50 @@
 import { Component } from '@angular/core';
+import {NonNullableFormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'ui-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+  form=this.fb.group(
+    {
+      email:['',{validators:[Validators.required,Validators.email],updateOn:'blur'}],
+      password:['',[Validators.required,Validators.minLength(8)]]
+    }
+  )
+
+  constructor(private fb:NonNullableFormBuilder) {
+  }
+
+
+  get email()
+  {
+    return this.form.controls['email'];
+  }
+
+  get password()
+  {
+    return this.form.controls['password'];
+  }
+
+
+
+
+  register() {
+    const formValue=this.form.value;
+
+    this.form.patchValue({
+
+    })
+  }
+
+  reset() {
+
+    this.form.reset();
+
+    console.log(this.form.value);
+  }
+
+}
