c4d3cba053e0f619c515e1945eb26740ad085c8e
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-01-08 22:50:49 -0300

    feat:routes login by register

--- libs/ui/header/src/lib/lib.routes.ts
+++ libs/ui/header/src/lib/lib.routes.ts
@@ -1,5 +1,21 @@
 import { Route } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
 
 export const uiHeaderRoutes: Route[] = [
-  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+  { path: '', pathMatch: 'full', component: HeaderComponent },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
 ];
--- libs/ui/header/src/lib/ui-header.module.ts
+++ libs/ui/header/src/lib/ui-header.module.ts
@@ -3,16 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { uiHeaderRoutes } from './lib.routes';
 import { HeaderComponent } from './header/header.component';
-import {BrowserModule} from "@angular/platform-browser";
+import { BrowserModule } from '@angular/platform-browser';
 
-import {MaterialModule} from "@port-esdraskhan/material";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {FlexLayoutModule} from "@angular/flex-layout";
+import { MaterialModule } from '@port-esdraskhan/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(uiHeaderRoutes),BrowserModule,MaterialModule,BrowserAnimationsModule,FlexLayoutModule
-   ],
-  declarations: [HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(uiHeaderRoutes),
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+  ],
+  declarations: [HeaderComponent, RegisterComponent, LoginComponent],
   exports: [HeaderComponent],
 })
 export class UiHeaderModule {}
